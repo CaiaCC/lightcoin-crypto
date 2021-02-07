b805de1209da56ceaf32f9ec9c9d54e3a99c53ed
Caia Chuang <caiachuang@gmail.com>
2021-02-06 23:11:36 -0800

    Refactor.

--- index.js
+++ index.js
@@ -1,88 +1,71 @@
-// let balance = 500.00;
-
 class Account {
-
-  constructor(username) {
-    this.username = username;
-    this.transaction = [];
-  }
-
-  get balance() {
-    let balance = 0;
-    for (let t of this.transaction) {
-      balance += t.value;
+    constructor(username) {
+        this.username = username;
+        this.transactions = [];
     }
-    return balance;
-  }
 
-  addTransaction(transaction) {
-    this.transaction.push(transaction);
-  }
+    get balance() {
+        if (this.transactions.length === 0) return 0;
+        return this.transactions.reduce( (a , b) => a + b.value, 0);
+    }
 
+    addTransaction(transaction) {
+      if (this.commit()) return this.transactions.push(transaction);
+    }
 }
 
 class Transaction {
-  constructor(amount, account) {
-    this.amount = amount;
-    this.account = account;
-  }
+    constructor(amount, account) {
+      this.amount = amount;
+      this.account = account;
+    }
 
-  commit() {
-    if (!this.isAllowed()) return false;
+    isAllowed() {
+        // if (this.value < 0 && this.account.balance <= 0) {
+        //     return false;
+        // }
+        // return true;
+        // !better way
+        return this.account.balance - this.amount >= 0;
+      }
 
-    this.time = new Date();
-    this.account.addTransaction(this);
-  }
+    commit() {
+      if (!this.isAllowed()) {
+          console.log(`Transaction failed! You don't have enough money!`);
+          return false;
+      } else {
+          this.time = new Date();
+          this.account.addTransaction(this);
+          return true;
+      }
+
+    }
 }
 
-class Withdrawal extends Transaction{
-
-  get value() {
-    return -this.amount;
-  }
-
-  isAllowed() {
-    return (this.account.balance - this.amount <= 0);
-  }
-
+class Withdrawal extends Transaction {
+    get value() {
+        return -this.amount;
+    }
 }
 
-class Deposit extends Transaction{
-
-  get value() {
-    return this.amount;
-  }
-
-  isAllowed() {
-    return true;
-  }
+class Deposit extends Transaction {
+    get value() {
+        return this.amount;
+    }
 }
 
+const myAccount = new Account('snow-patrol');
 
+t1 = new Withdrawal (50.25, myAccount);
+t1.commit();
+console.log('Transaction 1: ', t1);
 
+t2 = new Withdrawal(9.99, myAccount);
+t2.commit();
+console.log('Transaction 2: ', t2);
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+t3 = new Deposit(120.0, myAccount);
+t3.commit();
+console.log('Transaction 3: ', t3);
 
-const myAccount = new Account();
-
-console.log('Starting Account Balance: ', myAccount.balance);
-
-console.log('Attempting to withdraw even $1 should fail...');
-const t1 = new Withdrawal(1.00, myAccount);
-console.log('Commit result:', t1.commit());
-console.log('Account Balance: ', myAccount.balance);
-
-console.log('Depositing should succeed...');
-const t2 = new Deposit(9.99, myAccount);
-console.log('Commit result:', t2.commit());
-console.log('Account Balance: ', myAccount.balance);
-
-console.log('Withdrawal for 9.99 should be allowed...');
-const t3 = new Withdrawal(9.99, myAccount);
-console.log('Commit result:', t3.commit());
-
-console.log('Ending Account Balance: ', myAccount.balance);
-console.log("Lookings like I'm broke again");
-
-console.log('Account Transaction History: ', myAccount.transactions);
+console.log("myAccount balance: ", myAccount.balance);
